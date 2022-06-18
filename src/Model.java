07a23f98003602e697fe30b1a1d4a0ded623e420
alexshep <68069463+alexshep@users.noreply.github.com>
2022-06-18 14:18:59 +0200

    initial shit

---
+++ src/Model.java
@@ -0,0 +1,17 @@
+import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Model {
+	public List<Vector3f> vertices = new ArrayList<Vector3f>();
+	public List<Vector3f> normals = new ArrayList<Vector3f>();
+	public List<Vector2f> texCoords = new ArrayList<Vector2f>();
+	public List<FaceTriangle> faces = new ArrayList<FaceTriangle>();
+	public List<FaceQuad> facesQuads = new ArrayList<FaceQuad>();	
+	public float size;
+
+	public Model() {
+	}
+}
